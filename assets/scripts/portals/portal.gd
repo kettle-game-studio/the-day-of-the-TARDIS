58a14e789019ad849202dfe0003aad60f4075e75
SID37 <1999login@mail.ru>
2024-01-04 01:55:37 +0300

    Add dalek model

--- assets/scripts/portals/portal.gd
+++ assets/scripts/portals/portal.gd
@@ -1,15 +1,17 @@
 @tool
 extends Node
 
-@export var portal_vieport: SubViewport
+@export var portal_viewport: SubViewport
 @export var mesh: MeshInstance3D
 
 signal teleport_activation(body: Area3D)
 
 func _ready():
+	if mesh == null || portal_viewport == null:
+		return
 	var material = mesh.get_surface_override_material(0) as ShaderMaterial
 	material = material.duplicate()
-	material.set_shader_parameter("portal_camera", portal_vieport.get_texture())
+	material.set_shader_parameter("portal_camera", portal_viewport.get_texture())
 	mesh.set_surface_override_material(0, material)
 
 func _area_body_entered(body: Area3D):
