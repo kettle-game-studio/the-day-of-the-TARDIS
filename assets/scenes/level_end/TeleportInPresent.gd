744831b412ac0e98160e64c0b583ebdd59757c0c
RobolabGs2 <slavikelk@yandex.ru>
2024-01-08 00:51:25 +0300

    Ending

---
+++ assets/scenes/level_end/TeleportInPresent.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+@export var level: AbstractLevel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(_body):
+	print_debug(_body)
+	var p = level.portal_controller
+	level.player.global_position += p._get_room_shift()
+	p.disable_portal()
+	level.player.state = PlayerController.State.INTRO
